9787f28fe7f5f9670b035a0e55b3a382897b25d5
gtulkn12 <gtulkn1@yahoo.com>
2021-04-23 17:26:31 -0400

    updates

--- Database/index.js
+++ Database/index.js
@@ -3,14 +3,16 @@ const db = [
     uid: "185643",
     name: "Eiffel tower",
     location: "Paris",
-    photo: "",
+    photo:
+      "https://media.istockphoto.com/photos/eiffel-tour-and-paris-cityscape-picture-id1133449890?k=6&m=1133449890&s=612x612&w=0&h=Eq8_p2SS4OcOxVUhHDFmZtZIAolCuy94Hhi03BP3W20=",
     description: "Romantic Place",
   },
   {
     uid: "185652",
     name: "The Needle",
     location: "Seattle",
-    photo: "",
+    photo:
+      "https://www.enr.com/ext/resources/Blogs/Evergreen/Needle.jpg?1447700722",
     description: "Dope restaurant",
   },
 ];
